f0dfdb4f0a554defc22fc660771c1d6bae524a45
Juho Siitonen <juho_tuomas@msn.com>
2023-12-17 02:38:08 +0200

    modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require('./app') 
 const config = require('./utils/config')
+const express = require('express')
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
+
+app.use(express.static('build'))
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
